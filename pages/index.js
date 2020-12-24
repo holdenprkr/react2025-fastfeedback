0e26e47d6040b19e008778390b900ebf1004b71c
Holden <parkerholdenw@gmail.com>
2020-12-24 16:36:26 -0600

    Implented chakra ui and removed default styling

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,31 @@
 import Head from 'next/head';
-import { auth } from "firebase";
-import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
+import { useAuth } from '@/lib/auth';
+import { Button, Code, Heading } from '@chakra-ui/react';
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Fast Feedback</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <Heading>
           Fast Feedback
-        </h1>
+        </Heading>
         
-        <div className={styles.description}>
-          Current user: <code>{auth.user ? auth.user.email : 'None'}</code>
+        <div>
+          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
         </div>
         {auth.user ? (
-          <button onClick={(e) => auth.signout()}>Sign Out</button>
+          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
         ) : (
-          <button onClick={(e) => auth.signinWithGithub()}>Sign In</button>
+          <Button onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
         )}
        
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
